532c5e1b5c01d35b80e5e8553fab23dc04b0ec2a
Raphaël Candelier <raphael.candelier@upmc.fr>
2015-07-11 23:38:39 +0200

    Correct version of the shortcut code

--- +ML/+Plugins/+Projects/shortcuts.m
+++ +ML/+Plugins/+Projects/shortcuts.m
@@ -1,56 +1,26 @@
-function shortcuts(mode)
-%ML.Project.rm Remove project
-%   P = ML.PROJECT.RM(PNAME) Removes the project PNAME from the project.
-%   list.
+function out = shortcuts()
+%ML.Plugins.Projects.shortcuts Projects plugin shortcuts
 %
-%   See also ML.projects, ML.Project.new.
-%
-%   Reference page in Help browser: <a href="matlab:doc ML.Project.rm">doc ML.Project.rm</a>
-%   <a href="matlab:doc ML">MLab documentation</a>
+%   See also ML.projects, ML.config
 
-% --- Parameters ----------------------------------------------------------
-
-tag = 'Projects';
-scc = 'ML.Projects;';
-
-% -------------------------------------------------------------------------
-
-switch mode
-    
-    case 'disp'
-        
-        S = com.mathworks.mlwidgets.shortcuts.ShortcutUtils;
-        
-        % Add new category (of not existing)
-        if ~ismember('MLab', cell(S.getShortcutCategories))
-            S.addNewCategory('MLab');
-        end
-        
-        % List installed shortcuts
-        sl = arrayfun(@char, S.getShortcutsByCategory('MLab').toArray, ...
-            'UniformOutput', false);
-        
-        if ismember(tag, sl), s = 'Yes';
-        else s = 'No'; end
-        
-        fprintf('    <a href="matlab: ML.config(''%s'', ''%s'', ''toggle'');">%s</a>\t%s\n', ...
-            'shortcuts', tag, s, tag);
-        
-    case 'toggle'
-                
-        S = com.mathworks.mlwidgets.shortcuts.ShortcutUtils;
-            
-        if ismember(tag, arrayfun(@char, S.getShortcutsByCategory('MLab').toArray, ...
-                'UniformOutput', false))
-            S.removeShortcut('MLab', tag);
-        else
-            
-            % --- Get config
-            config = ML.Config.get;
-            
-            S.addShortcutToBottom(tag, ...
-                scc, [config.path 'Images' filesep 'Shortcuts/' tag '.png'], ...
-                'MLab', 'true');
-        end
-        
+out = struct('value', bool2str('get:shortcut:projects|Projects'), ...
+        'desc', 'Projects selector', ...
+        'cmd', 'r', ...
+        'action', 'toggle:shortcut:projects|Projects,menu:shortcuts');
 end
+ 
+% -------------------------------------------------------------------------
+function out = bool2str(b)
+
+res = ML.Config.action(b);
+if isstruct(res)
+    res = res.value;
+end
+
+if res
+    out = 'Yes';
+else
+    out = 'No';
+end
+end
+
